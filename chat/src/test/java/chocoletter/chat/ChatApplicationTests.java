de9d76218a0207dd0f927dfef5f1b85949cf6ba4
Jun2-Lee <ljh19980823@gmail.com>
2025-01-27 16:29:06 +0900

    [SETTING] 프로젝트 생성

---
+++ chat/src/test/java/chocoletter/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chocoletter.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
