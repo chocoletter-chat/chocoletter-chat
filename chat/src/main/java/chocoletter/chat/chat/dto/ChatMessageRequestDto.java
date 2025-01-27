42569dbbe75ad78054e4a6965494b0b04f346461
Jun2-Lee <ljh19980823@gmail.com>
2025-01-27 17:19:06 +0900

    #1 [FEAT] MongoDB 연결 테스트를 위한 테스트 컨트롤러, Dto, Service 작성

--- chat/src/main/java/chocoletter/chat/chat/dto/ChatMessageRequestDto.java
+++ chat/src/main/java/chocoletter/chat/chat/dto/ChatMessageRequestDto.java
@@ -1,4 +1,9 @@
 package chocoletter.chat.chat.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ChatMessageRequestDto {
+    private Long senderId;
+    private String content;
 }
--- chat/src/main/java/chocoletter/chat/chat/service/ChatMessageService.java
+++ chat/src/main/java/chocoletter/chat/chat/service/ChatMessageService.java
@@ -1,4 +1,15 @@
 package chocoletter.chat.chat.service;
 
+import chocoletter.chat.chat.domain.ChatMessage;
+import chocoletter.chat.chat.repository.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ChatMessageService {
+    private final ChatMessageRepository chatMessageRepository;
+    public void saveChatMessage(ChatMessage chatMessage) {
+        chatMessageRepository.save(chatMessage);
+    }
 }
