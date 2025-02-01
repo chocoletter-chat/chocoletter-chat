63f50b188b6a7a5b0b22ddbd78f87c6275a3b3f2
Jun2-Lee <ljh19980823@gmail.com>
2025-02-01 17:00:38 +0900

    #11 [FEAT] 컨트롤러 분리 & 채팅 내역 조회 컨트롤러 작성

--- chat/src/main/java/chocoletter/chat/api/chat/controller/ChatRestController.java
+++ chat/src/main/java/chocoletter/chat/api/chat/controller/ChatRestController.java
@@ -1,4 +1,26 @@
 package chocoletter.chat.api.chat.controller;
 
+import chocoletter.chat.api.chat.dto.response.ChatMessagesResponseDto;
+import chocoletter.chat.api.chat.service.ChatMessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/chat")
 public class ChatRestController {
+    private final ChatMessageService chatMessageService;
+
+    @GetMapping("/{roomId}/all")
+    public ResponseEntity<?> findChatMessages(@PathVariable("roomId") String roomId,
+                                              @RequestParam(defaultValue = "0") Integer page,
+                                              @RequestParam(defaultValue = "20") Integer size) {
+        ChatMessagesResponseDto chatMessages = chatMessageService.findChatMessages(roomId, page, size);
+        return ResponseEntity.ok(chatMessages);
+    }
 }
