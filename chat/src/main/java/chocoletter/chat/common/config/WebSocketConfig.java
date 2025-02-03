842402fdc188b2ec1cf003217811b8f538f8e838
yeseul106 <20191037@sungshin.ac.kr>
2025-02-03 23:29:11 +0900

    #20 [ADD] CORS 관련 처리 config 추가

--- chat/src/main/java/chocoletter/chat/common/config/WebSocketConfig.java
+++ chat/src/main/java/chocoletter/chat/common/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package chocoletter.chat.common.config;
 
+import chocoletter.chat.common.interceptor.StompInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,11 +11,20 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompInterceptor stompInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic"); // 브로커가 메시지 보내는 곳
         registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 메시지를 보낼 곳
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        // stompHandler를 인터셉터로 등록하여 STOMP 메시지 핸들링을 수행
+        registration.interceptors(stompInterceptor);
     }
 
     @Override
