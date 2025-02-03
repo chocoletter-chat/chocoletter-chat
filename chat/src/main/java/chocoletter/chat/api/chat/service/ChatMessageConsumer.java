3993465c49cbed007b2606362ec480f2a70e6201
yeseul106 <20191037@sungshin.ac.kr>
2025-02-03 23:28:32 +0900

    #20 [FEAT] service 로직 구현

--- chat/src/main/java/chocoletter/chat/api/chat/service/ChatMessageConsumer.java
+++ chat/src/main/java/chocoletter/chat/api/chat/service/ChatMessageConsumer.java
@@ -1,8 +1,11 @@
 package chocoletter.chat.api.chat.service;
 
 import chocoletter.chat.api.chat.domain.ChatMessage;
+import chocoletter.chat.api.chat.domain.MessageType;
 import chocoletter.chat.api.chat.dto.request.ChatMessageRequestDto;
 import chocoletter.chat.api.chat.dto.response.ChatMessageResponseDto;
+import chocoletter.chat.common.exception.ErrorMessage;
+import chocoletter.chat.common.exception.InternalServerException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
@@ -17,36 +20,56 @@ public class ChatMessageConsumer {
 
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatMessageService chatMessageService;
+    private final ChatRoomService chatRoomService;
 
     @KafkaListener(topics = "${spring.kafka.topic.chat}", groupId = "${spring.kafka.consumer.group-id}")
     public void consumeMessage(String message) {
         // 메시지를 WebSocket으로 전송
-        ChatMessageRequestDto chatMessageRequestDto = parseMessage(message);
-        String topic = "/topic/" + chatMessageRequestDto.getRoomId();
+        ChatMessageResponseDto chatMessageResponseDto = parseMessage(message);
+        String topic = "/topic/" + chatMessageResponseDto.roomId();
+
+        if (chatMessageResponseDto.messageType().equals(MessageType.READ_STATUS)) {
+            ChatMessageResponseDto result = ChatMessageResponseDto.builder()
+                    .messageType(chatMessageResponseDto.messageType())
+                    .senderId(chatMessageResponseDto.senderId())
+                    .senderName(chatMessageResponseDto.senderName())
+                    .content(chatMessageResponseDto.content())
+                    .isRead(false)
+                    .createdAt(String.valueOf(LocalDateTime.now()))
+                    .build();
+            messagingTemplate.convertAndSend(topic, result);
+            return;
+        }
+
+        // 현재 채팅방에 접속중인 인원이 있는지 확인
+        boolean isAllConnected = chatRoomService.isAllConnected(chatMessageResponseDto.roomId());
+
         messagingTemplate.convertAndSend(topic, ChatMessageResponseDto.builder()
-                .senderId(chatMessageRequestDto.getSenderId())
-                .senderName(chatMessageRequestDto.getSenderName())
-                .content(chatMessageRequestDto.getContent())
-                .isRead(false)
+                .messageType(chatMessageResponseDto.messageType())
+                .senderId(chatMessageResponseDto.senderId())
+                .senderName(chatMessageResponseDto.senderName())
+                .content(chatMessageResponseDto.content())
+                .isRead(isAllConnected)
                 .createdAt(String.valueOf(LocalDateTime.now()))
                 .build());
 
         // MongoDB에 메시지 저장
         chatMessageService.saveChatMessage(ChatMessage.builder()
-                .senderId(chatMessageRequestDto.getSenderId())
-                .senderName(chatMessageRequestDto.getSenderName())
-                .roomId(chatMessageRequestDto.getRoomId())
-                .content(chatMessageRequestDto.getContent())
+                .senderId(chatMessageResponseDto.senderId())
+                .senderName(chatMessageResponseDto.senderName())
+                .roomId(chatMessageResponseDto.roomId())
+                .content(chatMessageResponseDto.content())
+                .isRead(isAllConnected)
                 .build());
     }
 
-    private ChatMessageRequestDto parseMessage(String message) {
+    private ChatMessageResponseDto parseMessage(String message) {
         // JSON 문자열을 DTO로 변환
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.readValue(message, ChatMessageRequestDto.class);
+            return objectMapper.readValue(message, ChatMessageResponseDto.class);
         } catch (JsonProcessingException e) {
-            throw new RuntimeException("Failed to parse message", e);
+            throw new InternalServerException(ErrorMessage.ERR_PARSING_MESSAGE);
         }
     }
 }
