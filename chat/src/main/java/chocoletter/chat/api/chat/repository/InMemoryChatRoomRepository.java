3dbfd68d70cfe6108235970d0399372012c79776
yeseul106 <20191037@sungshin.ac.kr>
2025-02-03 23:28:08 +0900

    #20 [FIX] mongodb에서 isRead 읽음 처리하는 쿼리 오류 수정

---
+++ chat/src/main/java/chocoletter/chat/api/chat/repository/InMemoryChatRoomRepository.java
@@ -0,0 +1,32 @@
+package chocoletter.chat.api.chat.repository;
+
+import lombok.Getter;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+@Getter
+public class InMemoryChatRoomRepository {
+
+    private final Map<String, List<String>> chatRooms = new ConcurrentHashMap<>(); // key : roomId, value : 멤버 id 리스트
+
+    // 특정 roomId에 접속한 멤버 리스트 가져오기
+    public List<String> findOnlineMembers(String roomId) {
+        return chatRooms.getOrDefault(roomId, new ArrayList<>());
+    }
+
+    // 채팅방 저장 (입장 내역 추가)
+    public void save(String roomId, String memberId) {
+        chatRooms.computeIfAbsent(roomId, k -> new ArrayList<>()).add(memberId);
+    }
+
+    // 채팅방 삭제 (퇴장 시)
+    public void remove(String roomId, String memberId) {
+        chatRooms.computeIfPresent(roomId, (key, rooms) -> {
+            rooms.removeIf(id -> Objects.equals(id, memberId));
+            return rooms.isEmpty() ? null : rooms;
+        });
+    }
+}
+
