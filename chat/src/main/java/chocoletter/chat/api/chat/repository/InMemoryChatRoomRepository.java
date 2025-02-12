5104db1fafee5379a2c4c99026bbb689650b022c
yeseul106 <20191037@sungshin.ac.kr>
2025-02-12 19:36:43 +0900

    [FIX] chatRoom 자료형 수정

--- chat/src/main/java/chocoletter/chat/api/chat/repository/InMemoryChatRoomRepository.java
+++ chat/src/main/java/chocoletter/chat/api/chat/repository/InMemoryChatRoomRepository.java
@@ -5,20 +5,23 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Repository
-@Getter
 public class InMemoryChatRoomRepository {
 
-    private final Map<String, List<String>> chatRooms = new ConcurrentHashMap<>(); // key : roomId, value : 멤버 id 리스트
+    private final Map<String, Set<String>> chatRooms = new ConcurrentHashMap<>(); // key : roomId, value : 멤버 id 리스트
 
     // 특정 roomId에 접속한 멤버 리스트 가져오기
     public List<String> findOnlineMembers(String roomId) {
-        return chatRooms.getOrDefault(roomId, new ArrayList<>());
+        return new ArrayList<>(chatRooms.getOrDefault(roomId, Collections.emptySet()));
     }
 
     // 채팅방 저장 (입장 내역 추가)
     public void save(String roomId, String memberId) {
-        chatRooms.computeIfAbsent(roomId, k -> new ArrayList<>()).add(memberId);
+        chatRooms.computeIfAbsent(roomId, k -> Collections.newSetFromMap(new ConcurrentHashMap<>())).add(memberId);
         System.out.println("========== after connect chat room ==========");
         System.out.println(chatRooms);
         System.out.println("========== after connect members ==========");
@@ -26,13 +29,11 @@ public class InMemoryChatRoomRepository {
 
     // 채팅방 삭제 (퇴장 시)
     public void remove(String roomId, String memberId) {
-        chatRooms.computeIfPresent(roomId, (key, rooms) -> {
-            rooms.removeIf(id -> Objects.equals(id, memberId));
-            return rooms.isEmpty() ? null : rooms;
-        });
+        chatRooms.getOrDefault(roomId, Collections.emptySet()).remove(roomId);
         System.out.println("========== after remove chat room ==========");
         System.out.println(chatRooms);
         System.out.println("========== after remove members ==========");
     }
 }
+
 
